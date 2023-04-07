778f0659e8146672a687196667cddd666ff233fd
elsharqawy50 <abdelrahman.elsharqawy50@gmail.com>
2023-04-07 17:20:24 +0200

    update sidebar and add model component

--- components/layout/Sidebar.js
+++ components/layout/Sidebar.js
@@ -65,7 +65,7 @@ const navigation = [
 const Sidebar = () => {
   const [nav, setNav] = useState(navigation);
   const router = useRouter();
-  const { t } = useTranslation("dashboard");
+  const { t } = useTranslation("common");
 
   useEffect(() => {
     // every time the router changes, we need to update the nav style.
@@ -80,8 +80,22 @@ const Sidebar = () => {
   }, [router]);
 
   return (
-    <div className="flex w-14 flex-col border-none bg-blue-900 text-white transition-all duration-300 hover:w-64 dark:bg-gray-900 md:w-64">
-      <div className="flex flex-grow flex-col justify-between overflow-y-auto overflow-x-hidden">
+    <div className="sidebar flex w-14 flex-col border-none bg-blue-900 text-white transition-all duration-300 hover:w-64 dark:bg-gray-900 md:w-64">
+      <div className="flex flex-grow flex-col overflow-y-auto overflow-x-hidden">
+        <div className="mb-3 pt-5">
+          <div className="sidebar__image-box relative mx-auto mb-4 hidden h-28 w-28 cursor-pointer overflow-hidden rounded-full  shadow-lg md:block">
+            <img
+              src="/images/building-1.jpg"
+              className="sidebar__image block h-full scale-125 object-cover object-center transition-all duration-500"
+            />
+            <span className="sidebar__edit translate-y-1/5 absolute top-1/2 left-1/2 -translate-x-1/2 text-center text-sm opacity-0 transition-all duration-500">
+              {t("edit your account")}
+            </span>
+          </div>
+          <div className="sidebar__username mx-auto text-center capitalize md:block">
+            abdelrahman
+          </div>
+        </div>
         <ul className="flex flex-col space-y-1 py-4">
           <li className="hidden px-5 md:block">
             <div className="flex h-8 flex-row items-center">
@@ -142,7 +156,7 @@ const Sidebar = () => {
               </li>
             ))}
         </ul>
-        <p className="mb-14 hidden px-5 py-3 text-center text-xs md:block">
+        <p className="mb-14 mt-auto hidden px-5 py-3 text-center text-xs md:block">
           Copyright @2023
         </p>
       </div>
