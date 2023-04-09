7b88d94432b68a5859779249eb1f2dd705e995a0
elsharqawy50 <abdelrahman.elsharqawy50@gmail.com>
2023-04-09 17:19:58 +0200

    update translate

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "ar",
     locales: ["en", "ar"],
+    localeDetection: false,
+    localePath: path.resolve("./public/locales"),
   },
 };
