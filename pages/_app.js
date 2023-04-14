0dc2e140515997242ea3a532876a75e0b292175d
Abdo2711-coder <abdelrahmandiv@gmail.com>
2023-04-14 18:12:11 +0200

    progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,47 @@
-import React from 'react';
-import { appWithTranslation } from "next-i18next";
+import React, { useCallback, useLayoutEffect } from 'react';
 import Head from "next/head";
-import "styles/globals.scss";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
-import Layout from "components/layout/Layout";
+import PropTypes from "prop-types"
+import { appWithTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import NextNprogress from "nextjs-progressbar";
+
 import store from "../store";
 import { Provider } from "react-redux";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import GlobalSetting from "helper/settings/GlobalSetting";
+
 import { SessionProvider } from 'next-auth/react';
-import PropTypes from "prop-types"
+
+import Layout from "components/layout/Layout";
+import "styles/globals.scss";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import debounce from 'utils/debounce';
+import GlobalSetting from "helper/settings/GlobalSetting";
+
+const FONT_SIZE_BASE = 16;
+const FONT_SIZE_RATIO = 0.1122 / 3;
 
 function MyApp({ Component, pageProps }) {
+  const handleResize = useCallback(debounce(() => {
+    const fontSize =
+      window.innerWidth < 425
+        ? `${(window.innerWidth * FONT_SIZE_RATIO).toFixed(1)}px`
+        : `${FONT_SIZE_BASE}px`;
+    document.documentElement.style.fontSize = fontSize;
+  }, 100), []);
+
+  useLayoutEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("orientationchange", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("orientationchange", handleResize);
+    };
+  }, [handleResize]);
+
   const getLayout =
     Component.getLayout || ((page) => <Layout>{page}</Layout>);
   return (
@@ -26,6 +55,15 @@ function MyApp({ Component, pageProps }) {
           />
           <title>Real State Management</title>
         </Head>
+        <NextNprogress
+          color="#2563EB"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+          showSpinner={false}
+
+        />
         <ToastContainer
           position="top-center"
           autoClose={4000}
